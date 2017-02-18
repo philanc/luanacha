a9d427c8a92ef5292bf81249a0731f3d37110a1e
philanc <philanc@gmail.com>
2017-02-17 21:39:44 -0500

    ae_lock/unlock

--- src/luanacha.c
+++ src/luanacha.c
@@ -5,13 +5,67 @@
 
 luanacha
 
-This is a Lua wrapper for the Monocypher library by Loup Vaillant.
+This is a Lua library wrapping the Monocypher library by Loup Vaillant.
 http://loup-vaillant.fr/projects/monocypher/
+
+The functions keep as much as possible the same name as in  Monocypher 
+(without the "crypto_" prefix)
 
 luanachaAPI:
 
-func(...)
-	description...
+randombytes(n)
+	return a string containing n random bytes
+	
+--- Authenticated encryption
+
+ae_lock
+	authenticated encryption
+	with an optional prefix prepended to the encrypted text
+
+ae_unlock
+	authenticated decryption
+	with an optional offset for the start of the encrypted text
+
+--- Curve25519-based key exchange
+
+x25519_keypair
+	generates a pair of curve25519 keys (secret key, public key)
+
+x25519_public_key
+	return the public key associated to a secret key
+
+lock_key
+	DH key exchange. Return a session key
+
+--- Blake2b cryptographic hash
+
+blake2b_init
+	initialize and return a blake2b context object
+
+blake2b_update
+	update the hash with a new text fragment
+
+blake2b_final
+	return the final value of the hash
+
+blake2b
+	compute the hash of a string (convenience function)
+
+
+
+--- Ed25519 signature
+
+ed25519_keypair
+	generates a pair of ed25519 signature keys (secret key, public key)
+
+ed25519_public_key
+	return the public key associated to a secret key
+
+ed25519_sign
+	sign a text with a secret key
+
+ed25519_check
+	check a text signature with a public key
 
 */
 
@@ -51,17 +105,102 @@ func(...)
 //----------------------------------------------------------------------
 // lua binding   (all LuaNacha exposed functions are prefixed with "ln_")
 
+# define LERR(msg) return luaL_error(L, msg)
 
+#define NONCEBYTES 24
+#define KEYBYTES 32
+
+
+extern void randombytes(unsigned char *x,unsigned long long xlen); 
+
+static int ln_randombytes(lua_State *L) {
+	
+    size_t bufln; 
+	lua_Integer li = luaL_checkinteger(L, 1);  // 1st arg
+	bufln = (size_t) li;
+    unsigned char *buf = malloc(bufln); 
+	randombytes(buf, li);
+    lua_pushlstring (L, buf, bufln); 
+    free(buf);
+	return 1;
+}//randombytes()
 
 //----------------------------------------------------------------------
-// 
+// authenticated encryption
+
+static int ln_ae_lock(lua_State *L) {
+	// Lua API: ae_lock(k, n, m [, pfx])
+	//  k: key string (32 bytes)
+	//  n: nonce string (24 bytes)
+	//	m: message (plain text) string 
+	//  pfx: optional prefix string - prepended to the encrypted text
+	//     - pfx length should be a multiple of 8 for alignment
+	//  return encrypted text string
+	int r;
+	size_t mln, nln, kln, pfxln, bufln;
+	const char *k = luaL_checklstring(L,1,&kln);
+	const char *n = luaL_checklstring(L,2,&nln);	
+	const char *m = luaL_checklstring(L,3,&mln);	
+	const char *pfx = luaL_optlstring(L,4,"",&pfxln);
+	printf("pfxln: %d\n", pfxln);
+	if (nln != NONCEBYTES) LERR("bad nonce size");
+	if (kln != KEYBYTES) LERR("bad key size");
+	if ((pfxln % 8) != 0) LERR("bad prefix size");
+	bufln = mln + 16 + pfxln;
+	unsigned char * buf = malloc(bufln);
+	crypto_ae_lock(buf+pfxln, k, n, m, mln);
+	if (pfxln > 0) {
+		memcpy(buf, pfx, pfxln);
+	}
+	lua_pushlstring (L, buf, bufln); 
+	free(buf);
+	return 1;
+} // ae_lock()
+
+static int ln_ae_unlock(lua_State *L) {
+	// Lua API: ae_unlock(k, n, c [, i])
+	//  k: key string (32 bytes)
+	//  n: nonce string (24 bytes)
+	//	c: encrypted message string 
+	//  i: optional offset of the start of the encrypted text in c
+	//     default value is 0 - useful if c starts with a prefix
+	//  return plain text string or (nil, error msg if MAC is not valid)
+	int r = 0;
+	size_t cln, nln, kln, mln;
+	const char *k = luaL_checklstring(L,1,&kln);
+	const char *n = luaL_checklstring(L,2,&nln);	
+	const char *c = luaL_checklstring(L,3,&cln);	
+	int i = luaL_optinteger(L,4, 0);	
+	if (nln != NONCEBYTES) LERR("bad nonce size");
+	if (kln != KEYBYTES) LERR("bad key size");
+	
+	unsigned char * buf = malloc(cln);
+	mln = cln - i - 16;
+	r = crypto_ae_unlock(buf, k, n, c+i, mln);
+	if (r != 0) { 
+		free(buf); 
+		lua_pushnil (L);
+		lua_pushliteral(L, "unlock error");
+		return 2;         
+	} 
+	// the first 32 bytes should be null. ignore them
+	// plain is 16 byte shorter than encrypted (the 16-byte MAC)
+	lua_pushlstring (L, buf, mln); 
+	free(buf);
+	return 1;
+} // ln_ae_unlock()
+
+
+
 
 
 //------------------------------------------------------------
 // lua library declaration
 //
 static const struct luaL_Reg luanachalib[] = {
-	{"fname", func},
+	{"randombytes", ln_randombytes},
+	{"ae_lock", ln_ae_lock},
+	{"ae_unlock", ln_ae_unlock},
 	
 	{NULL, NULL},
 };
